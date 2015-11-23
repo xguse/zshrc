2b0ce5e13f110893ebd19f8c1bb959a7b3a5098e
Gus Dunn <wadunn83@gmail.com>
2015-11-23 15:49:30 -0500

    fixed zshrc_louise.sh to work with SHITTY OLD ZSH on louise

--- zshrc_louise.sh
+++ zshrc_louise.sh
@@ -1,90 +1,7 @@
-#####################################################################
-####### Set up zsh plugins and theme ################################
-#####################################################################
-
-ZGEN=/home2/wd238/src/repos/git/zgen/zgen.zsh
-ZSHRC_BASE=/home2/wd238/src/repos/git/zshrc
-
-
-# Set name of the theme to load.
-ZSH_THEME="steeef" #"sporty_256" #"kphoen" #"candy" #"agnoster" "steeef" #"candy" #"agnoster"
-
-# load zgen
-source $ZGEN
-
-# check if there's no init script
-if ! zgen saved; then
-    echo "Creating a zgen save"
-
-    zgen oh-my-zsh
-
-    # General System
-    zgen oh-my-zsh plugins/archlinux
-    zgen oh-my-zsh plugins/systemd
-    zgen oh-my-zsh plugins/sudo
-    zgen oh-my-zsh plugins/command-not-found
-    zgen load zsh-users/zsh-history-substring-search
-    zgen load zsh-users/zsh-syntax-highlighting
-
-    # python
-    zgen oh-my-zsh plugins/pip
-    zgen oh-my-zsh plugins/python
-    # git
-    zgen oh-my-zsh plugins/git
-    zgen oh-my-zsh plugins/gitignore
-    zgen oh-my-zsh plugins/git-extras
-    zgen oh-my-zsh plugins/git-flow-avh
-
-    # ruby
-    zgen oh-my-zsh plugins/rvm
-    zgen oh-my-zsh plugins/ruby
-    zgen oh-my-zsh plugins/gem
-    zgen oh-my-zsh plugins/rake
-    zgen oh-my-zsh plugins/rbenv
-
-    # misc
-    zgen oh-my-zsh plugins/tmuxinator
-
-    # completions
-    zgen load zsh-users/zsh-completions src
-
-    # theme
-    zgen oh-my-zsh themes/$ZSH_THEME
-
-    # save all to init script
-    zgen save
-fi
-
-ZGEN_RESET_ON_CHANGE=(${HOME}/.zshrc ${HOME}/.zshrc.local)
 
 #####################################################################
-####### import common partials ######################################
+####### load modules stuff bc this computer's git is ancient ########
 #####################################################################
-
-source $ZSHRC_BASE/aliases_universal
-source $ZSHRC_BASE/aliases_tmux
-source $ZSHRC_BASE/aliases_git
-
-
-#####################################################################
-####### My config stuff #############################################
-#####################################################################
-
-## If no conda env is set: set it to the one below, otherwise do nothing.
-if [[ ${CONDA_ENV_PATH} == '' ]]; then
-    source $HOME/.anaconda/bin/activate stack2
-else
-    conda_env_name=(${(ps:/:)${CONDA_ENV_PATH}})
-    source $HOME/.anaconda/bin/activate $conda_env_name[-1]
-fi
-
-#### PBS stuff
-export QSCRIPTS="${HOME}/qsub_scripts"
-
-alias qme="qstat -u ${USER}"
-alias qI8="qsub -I -lnodes=1:ppn=8 -S $(which zsh)"
-alias qI16="qsub -I -lnodes=1:ppn=16 -S $(which zsh)"
-alias qI60="qsub -I -lnodes=1:ppn=60 -S $(which zsh)"
 
 ### Set Up my LOCAL `modules` and `easybuild` to be used
 # adjust line below if you're using a shell other than bash, check with 'echo $SHELL'
@@ -109,6 +26,103 @@ alias mdsh="md show"
 md load git/2.3.4-goolf-1.4.10-no-OFED
 md load EasyBuild/2.0.0
 # md load zlib/1.2.8-goolf-1.4.10-no-OFED
+
+
+
+#####################################################################
+####### Set up zsh plugins and theme ################################
+#####################################################################
+
+ZGEN=/home2/wd238/src/repos/git/zgen/zgen.zsh
+ZSHRC_BASE=/home2/wd238/src/repos/git/zshrc
+
+fpath=($ZSHRC_BASE/zsh_supplements/Functions/Misc $fpath)
+
+# Set name of the theme to load.
+ZSH_THEME="sporty_256" #"kphoen" #"candy" #"agnoster" "steeef" #"candy" #"agnoster"
+
+# load zgen
+source $ZGEN
+
+# check if there's no init script
+if ! zgen saved; then
+    echo "Creating a zgen save"
+
+    zgen oh-my-zsh
+
+    # # General System
+    # zgen oh-my-zsh plugins/archlinux
+    # zgen oh-my-zsh plugins/systemd
+    # zgen oh-my-zsh plugins/sudo
+    zgen oh-my-zsh plugins/command-not-found
+    zgen load zsh-users/zsh-history-substring-search
+    # zgen load zsh-users/zsh-syntax-highlighting
+
+    # python
+    zgen oh-my-zsh plugins/pip
+    zgen oh-my-zsh plugins/python
+
+    # git
+    zgen oh-my-zsh plugins/git
+    # zgen oh-my-zsh plugins/gitignore
+    # zgen oh-my-zsh plugins/git-extras
+    # zgen oh-my-zsh plugins/git-flow-avh
+
+    # ruby
+    zgen oh-my-zsh plugins/rvm
+    zgen oh-my-zsh plugins/ruby
+    zgen oh-my-zsh plugins/gem
+    zgen oh-my-zsh plugins/rake
+    zgen oh-my-zsh plugins/rbenv
+
+    # misc
+    zgen oh-my-zsh plugins/tmuxinator
+
+    # completions
+    # zgen load zsh-users/zsh-completions src
+
+    # theme
+    zgen oh-my-zsh themes/$ZSH_THEME
+
+    # save all to init script
+    zgen save
+fi
+
+# ZGEN_RESET_ON_CHANGE=(${HOME}/.zshrc ${HOME}/.zshrc.local)
+
+#####################################################################
+####### import common partials ######################################
+#####################################################################
+
+source $ZSHRC_BASE/aliases_universal
+source $ZSHRC_BASE/aliases_tmux
+source $ZSHRC_BASE/aliases_git
+
+
+#####################################################################
+####### My config stuff #############################################
+#####################################################################
+
+
+
+
+## If no conda env is set: set it to the one below, otherwise do nothing.
+if [[ ${CONDA_ENV_PATH} == '' ]]; then
+    source $HOME/.anaconda/bin/activate none
+else
+    conda_env_name=(${(ps:/:)${CONDA_ENV_PATH}})
+    source $HOME/.anaconda/bin/activate $conda_env_name[-1]
+fi
+
+#### PBS stuff
+export QSCRIPTS="${HOME}/qsub_scripts"
+
+alias qme="qstat -u ${USER}"
+alias qI8="qsub -I -lnodes=1:ppn=8 -S $(which zsh)"
+alias qI16="qsub -I -lnodes=1:ppn=16 -S $(which zsh)"
+alias qI60="qsub -I -lnodes=1:ppn=60 -S $(which zsh)"
+
+
 
 
 #### Anaconda stuff
