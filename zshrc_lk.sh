238336bf711db393e78848b2571afce8ab96239f
Gus Dunn <wadunn83@gmail.com>
2016-02-01 13:39:32 -0500

    added zshr_lk

---
+++ zshrc_lk.sh
@@ -0,0 +1,71 @@
+#####################################################################
+####### Set up zsh plugins and theme ################################
+#####################################################################
+
+ZGEN=/home/lk55/.gus/zgen/zgen.zsh
+ZSHRC_BASE=/home/lk55/.gus/zshrc
+
+
+# Set name of the theme to load.
+ZSH_THEME="sporty_256" #"sporty_256" #"kphoen" #"candy" #"agnoster" "steeef" #"candy" #"agnoster"
+
+# load zgen
+source $ZGEN
+
+# check if there's no init script
+if ! zgen saved; then
+    echo "Creating a zgen save"
+
+    zgen oh-my-zsh
+
+    # General System
+    # zgen oh-my-zsh plugins/archlinux
+    # zgen oh-my-zsh plugins/systemd
+    zgen oh-my-zsh plugins/sudo
+    zgen oh-my-zsh plugins/command-not-found
+    zgen load zsh-users/zsh-history-substring-search
+    zgen load zsh-users/zsh-syntax-highlighting
+
+    # python
+    zgen oh-my-zsh plugins/pip
+    zgen oh-my-zsh plugins/python
+    # git
+    zgen oh-my-zsh plugins/git
+    zgen oh-my-zsh plugins/gitignore
+    zgen oh-my-zsh plugins/git-extras
+    zgen oh-my-zsh plugins/git-flow-avh
+
+    # ruby
+    zgen oh-my-zsh plugins/rvm
+    zgen oh-my-zsh plugins/ruby
+    zgen oh-my-zsh plugins/gem
+    zgen oh-my-zsh plugins/rake
+    zgen oh-my-zsh plugins/rbenv
+
+    # misc
+    zgen oh-my-zsh plugins/tmuxinator
+
+    # completions
+    zgen load zsh-users/zsh-completions src
+
+    # theme
+    zgen oh-my-zsh themes/$ZSH_THEME
+
+    # save all to init script
+    zgen save
+fi
+
+ZGEN_RESET_ON_CHANGE=(${HOME}/.zshrc ${HOME}/.zshrc.local)
+
+#####################################################################
+####### import common partials ######################################
+#####################################################################
+
+source $ZSHRC_BASE/aliases_universal
+source $ZSHRC_BASE/aliases_tmux
+source $ZSHRC_BASE/aliases_git
+
+
+#####################################################################
+####### My config stuff #############################################
+#####################################################################
